b8a108fa656e1e6788af6c65d98c6569df87cd3f
Nicholas Park <nicholaspark09@gmail.com>
2023-12-12 16:06:42 -0500

    Rename to awsgorocket

--- database/database_helper.go
+++ database/database_helper.go
@@ -6,7 +6,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
-	"github.com/nicholaspark09/awsgorocket/github.com/nicholaspark09/awsgorocket/converter"
+	"github.com/nicholaspark09/awsgorocket/converter"
 	"log"
 )
 
